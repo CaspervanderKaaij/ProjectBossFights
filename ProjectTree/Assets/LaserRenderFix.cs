5a4af7477cdd4940dc3eac89854f7c0817d75d51
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2020-01-07 21:08:31 +0100

    fixes

---
+++ ProjectTree/Assets/LaserRenderFix.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserRenderFix : MonoBehaviour {
+
+    void Start () {
+        transform.GetComponent<MeshFilter>().mesh.bounds = new Bounds (Vector3.zero, Vector3.one * 2000);
+        Destroy(this);
+    }
+}
