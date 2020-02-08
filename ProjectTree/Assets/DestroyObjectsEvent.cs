1406c7650198f6befa298f7fbd98be5ab3032178
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2020-02-08 20:04:07 +0100

    phase 3 orbs

---
+++ ProjectTree/Assets/DestroyObjectsEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DestroyObjectsEvent : MonoBehaviour {
+    [SerializeField] GameObject[] objects;
+    [SerializeField] UnityEvent ev;
+
+    void LateUpdate () {
+        bool willDoEv = true;
+        for (int i = 0; i < objects.Length; i++) {
+            if (objects[i] != null) {
+                willDoEv = false;
+            }
+        }
+
+        if (willDoEv == true) {
+            Destroy (this);
+            ev.Invoke ();
+        }
+    }
+
+    public void DestroyMe () {
+        Destroy (gameObject);
+    }
+}
---
+++ ProjectTree/Assets/MikaBossOrbRandomPath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MikaBossOrbRandomPath : MonoBehaviour
+{
+    [SerializeField] Transform[] posses;
+    [SerializeField] float speed = 5;
+    int curGoal = 0;
+    void Start() 
+    {
+        curGoal = Random.Range(0,posses.Length);
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position,posses[curGoal].position,Time.deltaTime * speed);
+        if(Vector3.Distance(transform.position, posses[curGoal].position) < 1){
+            curGoal = Random.Range(0,posses.Length);
+        }
+    }
+}
